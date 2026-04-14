a25dd8bdd07a512b9135b4ef8877b4811cb9f7e0
KIRPI4228 <kirpi4t@gmail.com>
2026-04-14 22:06:32 +0300

    Implement log4js logger & Edit .gitignore for /logs folder

--- src/Logger.js
+++ src/Logger.js
@@ -1,23 +1,48 @@
+import log4js from 'log4js'
+
 export default class Logger {
+    static {
+        log4js.configure({
+            appenders: {
+                everything: {
+                    type: 'file',
+                    filename: `logs/${Date.now()}.log`,
+                    maxLogSize: 10485760,
+                    backups: 2,
+                    compress: true
+                }
+            },
+            categories: {
+                default: { appenders: ["everything"], level: "info" }
+            }
+        });
+    }
+
+    static #logger = log4js.getLogger('ProfSpoVerifier');
 
     static log = (text) => {
         console.log(this.#text(text, "LOG"));
+        Logger.#logger.log(text);
     }
 
     static info = (text) => {
         console.info(this.#text(text, "INFO"));
+        Logger.#logger.info(text);
     }
 
     static warn = (text) => {
         console.warn(this.#text(text, "WARN"));
+        Logger.#logger.warn(text);
     }
 
     static error = (text) => {
         console.error(this.#text(text, "ERROR"));
+        Logger.#logger.error(text);
     }
 
     static debug = (text) => {
         console.debug(this.#text(text, "DEBUG"));
+        Logger.#logger.debug(text);
     }
 
     static #text = (text, type) => {
